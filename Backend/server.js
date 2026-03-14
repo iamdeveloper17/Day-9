26fdfb7299508cdbd2b429902408c6d8fb409b71
Amit Kumar <ramit5752@gmail.com>
2026-03-15 01:40:20 +0530

    Add two new changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,12 @@ const connectToDb = require("./src/config/database")
 
 connectToDb()
 
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+// app.listen(3000,()=>{
+//     console.log("server is running on port 3000")
+// })
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`)
 })
